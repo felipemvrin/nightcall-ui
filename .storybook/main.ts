339f37b19e74b6187748d7d551043d419c86b7f1
felipemvrin <felipemarin@gmail.com>
2026-08-07 11:30:29 -0400

    feat(branding): set nc favicon for web and storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,7 @@ const config: StorybookConfig = {
   stories: ['../apps/docs/src/**/*.stories.@(ts|mdx)'],
   addons: ['@storybook/addon-a11y', 'storybook/viewport'],
   previewHead: (head) => `${head}
-<link rel="icon" type="image/x-icon" href="/nc-favicon.ico" />`,
+<link rel="icon" type="image/svg+xml" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'%3E%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle' font-size='48'%3E🌌%3C/text%3E%3C/svg%3E" />`,
   framework: {
     name: '@storybook/angular',
     options: {},
