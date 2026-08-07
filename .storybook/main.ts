50a7522abf78ec707bd44ecce7cdab2046c4db94
felipemvrin <felipemarin@gmail.com>
2026-08-07 10:56:21 -0400

    chore(branding): unify favicon across web and storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,7 @@ const config: StorybookConfig = {
   stories: ['../apps/docs/src/**/*.stories.@(ts|mdx)'],
   addons: ['@storybook/addon-a11y', 'storybook/viewport'],
   previewHead: (head) => `${head}
-<link rel="icon" type="image/svg+xml" href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>%F0%9F%8C%8C</text></svg>' />`,
+<link rel="icon" type="image/x-icon" href="/nc-favicon.ico" />`,
   framework: {
     name: '@storybook/angular',
     options: {},
