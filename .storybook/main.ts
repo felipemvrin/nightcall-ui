6147af5d36b4e0259d1ed52a54f5b25aecba9b08
felipemvrin <felipemarin@gmail.com>
2026-08-11 15:37:54 -0400

    fix: adjust music player artwork sizing

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,26 @@
 import type { StorybookConfig } from '@storybook/angular';
 
+const favicon = `
+<link
+  rel="icon"
+  type="image/svg+xml"
+  href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'%3E%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle' font-size='48'%3E%F0%9F%8C%8C%3C/text%3E%3C/svg%3E"
+/>`;
+
 const config: StorybookConfig = {
   stories: ['../apps/docs/src/**/*.stories.@(ts|mdx)'],
   addons: ['@storybook/addon-a11y', 'storybook/viewport'],
-  previewHead: (head) => `${head}
-<link rel="icon" type="image/svg+xml" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'%3E%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle' font-size='48'%3E🌌%3C/text%3E%3C/svg%3E" />`,
+
+  managerHead: (head) =>
+    head.replace('<link rel="icon" type="image/x-icon" href="./favicon.ico" />', favicon),
+  previewHead: (head) => `${head}${favicon}`,
+
   framework: {
     name: '@storybook/angular',
     options: {},
   },
-  staticDirs: ['../apps/docs/public'],
-  webpackFinal: async (webpackConfig) => {
-    webpackConfig.performance = {
-      hints: false,
-    };
 
-    return webpackConfig;
-  },
+  staticDirs: ['../apps/docs/public'],
 };
 
 export default config;
